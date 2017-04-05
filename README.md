e7cecbc2e9003ac412d83021cc760c1cdd8f349f
dimitriwashere <dimitriwashere@gmail.com>
2017-04-05 16:08:50 -0400

    Refactor, modify README.md

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # Personify.
 
 - Configured to access the Personify. API endpoint at [http://personify-server.herokuapp.com/api](http://personify-server.herokuapp.com)
-- Built with JavaScript, JQuery, AJAX, HTML, CSS
+- Built with Ruby, JavaScript, JQuery, AJAX, HTML, CSS
 
 Example AJAX request:
 
+    ...
     $.ajax({
       method: "post",
       url: "https://personify-server.herokuapp.com/api/v1/people",
       data: {
-          name: name,
-          favoriteCity: favoriteCity
-        }
-    ...    
+        name: name,
+        favoriteCity: favoriteCity
+      },
+    ...
+    })    
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,28 +1,45 @@
 $(document).on('ready', function() {
   $("body").on("click", "#get_people_btn", getPeople)
+  $("body").on("click", ".person", getPerson)
   $("body").on("click", "#post_person_btn", postPerson)
   $("body").on("click", "#update_person_btn", updatePerson)
   $("body").on("click", "#delete_person_btn", deletePerson)
 });
 
 function getPeople(e) {
-  history.replaceState({}, document.title, ".");
   $.ajax({
     method: "get",
     url: "https://personify-server.herokuapp.com/api/v1/people",
-    error: function(data, textStatus) {
+    error: function(data) {
       $("#get_people").empty().append("Please try again.")
     },
-    success: function(data, textStatus) {
+    success: function(data) {
       $("#get_people").empty().append("<p>GET /people/:id</p>")
       data.forEach(function(person){
         var id = person.id
-        var name_link = $(document.createElement("a"))
-        name_link.attr('href', "https://personify-server.herokuapp.com/api/v1/people/" + parseInt(id))
-        name_link.attr('class', "people").attr('id', id).attr('target', '_blank')
-        name_link.text(person.name)
-        $("#get_people").append(name_link).append("<br>")
+        var name = $(document.createElement("ui"))
+        var favoriteCity = person.favoriteCity
+        name.attr('class', "person")
+        name.attr('id', id)
+        name.text(person.name)
+        $("#get_people").append(name).append("<br>")
       })
+    }
+  })
+}
+
+function getPerson(e) {
+  $.ajax({
+    method: "get",
+    url: "https://personify-server.herokuapp.com/api/v1/people/" + this.id,
+    error: function(data) {
+      $("#get_message").empty().append("Please try again.")
+    },
+    success: function(data) {
+      var nameText = "name: " + data.name
+      var favoriteCityText = "favoriteCity: " + data.favoriteCity
+      $("#get_message").empty().prepend("<br>").append("<h4>Person:</h4>").append(nameText).append("<br>").append(favoriteCityText)
+      window.setTimeout(function(){location.reload()},3000)
     }
   })
 }
@@ -35,29 +52,18 @@ function postPerson(e) {
     method: "post",
     url: "https://personify-server.herokuapp.com/api/v1/people",
     data: {
-        name: name,
-        favoriteCity: favoriteCity
-      },
-    error: function(data, textStatus) {
+      name: name,
+      favoriteCity: favoriteCity
+    },
+    error: function(data) {
       $("#post_message").empty().prepend("<br>").append("Please try again.")
       window.setTimeout(function(){location.reload()},2000)
     },
-    success: function(data, textStatus) {
-      if (data.errors){
-        var error_info = "<h4>Not Added:</h4>"
-          data.errors.forEach(function(info){
-            if (info.id == "favoriteCity"){
-              info.title = info.title.replace(/Favoritecity/i, 'Favorite city')
-            }
-            error_info += "<text style=font-weight:bold>" + info.id + ": " + "</text>"+ info.title + "<br>"
-          })
-        $("#post_message").empty().prepend("<br>").append(error_info)
-      } else {
-        var name_text = "name: " + data.name
-        var city_text = "favoriteCity: " + data.favoriteCity
-        $("#post_message").empty().prepend("<br>").append("<h4>Added:</h4>").append(name_text).append("<br>").append(city_text)
-        window.setTimeout(function(){location.reload()},3000)
-      }
+    success: function(data) {
+      var nameText = "name: " + data.name
+      var favoriteCityText = "favoriteCity: " + data.favoriteCity
+      $("#post_message").empty().prepend("<br>").append("<h4>Added:</h4>").append(nameText).append("<br>").append(favoriteCityText)
+      window.setTimeout(function(){location.reload()},3000)
     }
   })
 }
@@ -70,14 +76,14 @@ function updatePerson(e) {
     method: "put",
     url: "https://personify-server.herokuapp.com/api/v1/people/1",
     data: {
-        name: name,
-        favoriteCity: favoriteCity
-      },
-    error: function(data, textStatus) {
+      name: name,
+      favoriteCity: favoriteCity
+    },
+    error: function(data) {
       $("#update_message").empty().prepend("<br>").append("Looks like that person has been deleted.")
       window.setTimeout(function(){location.reload()},2000)
     },
-    success: function(data, textStatus) {
+    success: function(data) {
       location.reload();
     }
   })
@@ -88,11 +94,11 @@ function deletePerson(e) {
   $.ajax({
     method: "delete",
     url: "https://personify-server.herokuapp.com/api/v1/people/1",
-    error: function(data, textStatus){
+    error: function(data){
       $("#delete_message").empty().append("<br>Looks like that person has been deleted.")
       window.setTimeout(function(){location.reload()},2000)
     },
-    success: function(data, textStatus) {
+    success: function(data) {
       $("#delete_message").empty().prepend("<br>").append("<br>Person with an id of 1 has been deleted.")
       window.setTimeout(function(){location.reload()},2000)
     }
